0ad7731fc96e161873a581fab3717124762f4102
Serhii Fesenko <serhii.fsnk@gmail.com>
2021-06-03 23:15:28 +0300

    Improve hooks

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,45 @@
-import { useSelector } from "./store";
+import { useState, useCallback } from "react";
+import { useSelector, useDispatch, login, logout } from "./store";
+import api from "./api";
+
+export const useInput = (initialValue = "") => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return [value, onChange];
+};
+
+export const useLoginHandler = () => {
+  const dispatch = useDispatch();
+
+  const loginHandler = useCallback(
+    async (username, password) => {
+      try {
+        const response = await api.login(username, password);
+
+        dispatch(login(response));
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [dispatch]
+  );
+
+  return loginHandler;
+};
+
+export const useLogoutHandler = () => {
+  const dispatch = useDispatch();
+
+  const logoutHandler = useCallback(() => {
+    dispatch(logout());
+  }, [dispatch]);
+
+  return logoutHandler;
+};
 
 export const useIsAuthenticated = () => {
   const isAuthenticated = useSelector((store) => store.isAuthenticated);
